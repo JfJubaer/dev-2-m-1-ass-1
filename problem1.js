491828b54dd00eb1ee77dbf8d4b95ceb700768fb
JfJubaer <skk5727@gmail.com>
2024-03-02 15:07:55 +0600

    zubs as1-m2-p2

--- problem1.js
+++ problem1.js
@@ -1,7 +1,19 @@
-function repeater(str, num) {
-    if (num === void 0) { num = 3; }
-    for (var i = 1; i <= num; i++) {
-        console.log(str);
-    }
+// function repeater(str: string, num: number = 3): void {
+//   for (let i = 1; i <= num; i++) {
+//     console.log(str);
+//   }
+// }
+function ageCheck(arr) {
+    var newArr = [];
+    arr.map(function (a) {
+        if (a.age > 18) {
+            newArr.push(a);
+        }
+    });
+    return newArr;
 }
-repeater("Zubs");
+console.log(ageCheck([
+    { name: "zubs", age: 20 },
+    { name: "zub", age: 15 },
+    { name: "zuba", age: 115 },
+]));
--- problem1.ts
+++ problem1.ts
@@ -1,7 +1,32 @@
-function repeater(str: string, num: number = 3): void {
-  for (let i = 1; i <= num; i++) {
-    console.log(str);
-  }
-}
+// function repeater(str: string, num: number = 3): void {
+//   for (let i = 1; i <= num; i++) {
+//     console.log(str);
+//   }
+// }
 
-repeater("Zubs");
+// repeater("Zubs");
+
+// type Generic<T> = Array<T>;
+
+// type Obj = {
+//   name: string;
+//   age: number;
+// };
+
+// function ageCheck(arr: Generic<Obj>): Generic<Obj> {
+//   let newArr: Generic<Obj> = [];
+//   arr.map((a) => {
+//     if (a.age > 18) {
+//       newArr.push(a);
+//     }
+//   });
+//   return newArr;
+// }
+
+// console.log(
+//   ageCheck([
+//     { name: "zubs", age: 20 },
+//     { name: "zub", age: 15 },
+//     { name: "zuba", age: 115 },
+//   ])
+// );
