98e4a8e328e1ba835ce629ca292218626f1213ca
JfJubaer <skk5727@gmail.com>
2024-03-02 14:45:34 +0600

    zubs ass-1 m2 p-1

---
+++ problem1.js
@@ -0,0 +1,7 @@
+function repeater(str, num) {
+    if (num === void 0) { num = 3; }
+    for (var i = 1; i <= num; i++) {
+        console.log(str);
+    }
+}
+repeater("Zubs");
---
+++ problem1.ts
@@ -0,0 +1,7 @@
+function repeater(str: string, num: number = 3): void {
+  for (let i = 1; i <= num; i++) {
+    console.log(str);
+  }
+}
+
+repeater("Zubs");
